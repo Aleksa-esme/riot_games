ef353259c468a174e50f60555f421e15c9d97a51
Aleksa-esme <esme1990@mail.ru>
2021-05-13 08:26:05 +0300

    changed comments

--- JS/hero.js
+++ JS/hero.js
@@ -3,35 +3,27 @@
 let button = document.getElementById('button');
 
 button.onclick = function() {
-    //сформировать массив из всех героев на странице
-    let heroArray = Array.from(document.querySelectorAll('.hero'));
-    //определить у какого индекса в массиве сейчас active класс
-    let index = heroArray.findIndex(element => element.classList.contains('hero-active'));
+
+    let heroArray = Array.from(document.querySelectorAll('.hero')); //сформировать массив из всех героев на странице
+    let index = heroArray.findIndex(element => element.classList.contains('hero-active')); //определить у какого индекса в массиве сейчас active класс
  
-    //изменить active класс и инвертировать цвета
-    heroActive(heroArray, index);                      
-    //если убрали класс у последнего элемента в массиве, то переходим к нулевому элементу массива
-    if (index == heroArray.length - 1) {
+    heroActive(heroArray, index);     //изменить active класс и инвертировать цвета                 
+    
+    if (index == heroArray.length - 1) {  //если убрали класс у последнего элемента в массиве, то переходим к нулевому элементу массива
         index = 0;
-        heroActive(heroArray, index);
-    //если элемент массива не последний, то переходим к следующему                           
-    } else {
+        heroActive(heroArray, index);                           
+    } else {  //если элемент массива не последний, то переходим к следующему
         ++index;
         heroActive(heroArray, index);                                                    
     }
 }
 
 function heroActive(array, index) {
-    //поиск подблока скиллов у выбранного индекса
-    let heroSkillsBlock = array[index].querySelector('.hero__skills');
-    //формирование массива скилов в выбранном подблоке
-    let skillsArray = Array.from(heroSkillsBlock.children);
-    //поиск имени у выбранного индекса
-    let heroName = array[index].querySelector('.hero_name_img');
-    //формирование массива основных скилов
-    let mainSkills = Array.from(document.querySelectorAll('.main__skills_blocks'));
-    //формирование массива картинок скилов в основном блоке скиллов
-    let mainSkillsImages = Array.from(mainSkills[index].querySelectorAll('.main__skills-image'));
+    let heroSkillsBlock = array[index].querySelector('.hero__skills');  //поиск подблока скиллов у выбранного индекса
+    let skillsArray = Array.from(heroSkillsBlock.children);  //формирование массива скилов в выбранном подблоке
+    let heroName = array[index].querySelector('.hero_name_img');  //поиск имени у выбранного индекса
+    let mainSkills = Array.from(document.querySelectorAll('.main__skills_blocks'));  //формирование массива основных скилов
+    let mainSkillsImages = Array.from(mainSkills[index].querySelectorAll('.main__skills-image'));  //формирование массива картинок скилов в основном блоке скиллов
     
     
     //добавить или убрать active классы
